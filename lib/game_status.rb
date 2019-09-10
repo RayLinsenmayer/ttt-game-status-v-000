69f5c8c637aeae1d882b2833de5efd5b35da269b
Ray Linsenmayer <raymond_linsenmayer@hotmail.com>
2019-09-10 16:50:19 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,96 @@
+  # Previous code 
+# Helper Methods
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+board = [" "," "," "," "," "," "," "," "]
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+# Define your play method below
+
+def play(board)
+  counter = 0 
+  until counter == 9 
+   turn(board)
+   counter +=1 
+  end
+end
+
+def turn_count(board)
+  turns = 0 
+  board.each do |position_status|
+    if position_status == "X" || position_status == "O"
+        turns = turns + 1 
+    end
+  end
+  turns
+end
+
+
+def current_player(board)
+  if turn_count(board).even?
+    return "X"
+  else
+    return "O"
+  end  
+end
+
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+    [0,1,2], #top row
+    [3,4,5], #middle row
+    [6,7,8], #bottom row  
+    [0,3,6], #left column
+    [1,4,7], #middle column
+    [2,5,8], #right column
+    [0,4,8], #Top-Left to Bottom-Right diagonal
+    [2,4,6], #Top-Right to Bottom-Left diagonal
+  ]
+
+
+def won?(board)
+    WIN_COMBINATIONS.detect do |combo| 
+    board[combo[0]] == board[combo[1]] && board[combo[1]] == board[combo[2]] && position_taken?(board, combo[0])
+    end
+   
+end
+      
+
+
